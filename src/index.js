eed0d8bc1be17e0ef10af4e2a897cd1f474c6f31
kisyular <rellika@kisyula.com>
2023-04-07 22:28:50 +0300

    REACT APP - SETUP REDUX TOOLKIT

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import { store } from './app/store'
+import { Provider } from 'react-redux'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route path='/*' element={<App />} />
-			</Routes>
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/*' element={<App />} />
+				</Routes>
+			</BrowserRouter>
+		</Provider>
 	</React.StrictMode>
 )
 
